235a4be45adf6d65cf67230672d251c9a458c536
leehs1979 <leehs1979@naver.com>
2021-06-29 21:45:30 +0900

    Update

--- knservice/flow_initiator/app.py
+++ knservice/flow_initiator/app.py
@@ -17,27 +17,42 @@ class FlowInitiator(Resource):
         
         print('[START] FlowInitiator')
         
-        req_data = request.get_json()
-        print(req_data)
-        
-        #flowmanager_uri도 줄것
-        # TODO: 변경할 것
+        # input: 사용자 요청(시스템) -원래 # TODO: 하드코딩으로 몇개 만들어 두자. # http://172.16.1.110:17000/<id> 로 호출
+        # flow_nm -> flow_id로 매핑
+        # in_data
+        # run_job_id
+        # final_result_callback        
         '''
-        req_data = request.get_json()
-        targetURL = req_data['targetURL']
-        targetAPI = req_data['targetAPI']
-        headers = req_data['headers']
-        body = req_data['body']
-        print(targetURL)
-        print(targetAPI)
-        print(headers)
-        print(body)
-        json_data=json.dumps(body)
+        1. 테스트 - sync1 - sync2 - async1 - sync3 : 결과 - OK
 
-        res = requests.post(targetURL+targetAPI, headers=headers, data=json_data)
-        
-        return res.json()
+        {
+            "flow_id": "a138634c-29d1-43cb-9e41-99dbc911e777",
+            "in_data": { "type": 1 },
+            "api_seq": 0,
+            "run_job_id": "test1_runtime",
+            "final_result_callback": "http://172.16.1.110:19000/callback_post",
+            "service_dispatcher_uri": "http://172.16.1.110:18081",
+            "service_async_receiver_uri": "http://172.16.1.110:18082"
+        }
+
+
+        2. 테스트 - sync1 - sync2 - sync3 : 결과 - OK
+
+        {
+            "flow_id": "0af0714b-5896-4894-b0ce-ab02ac570608",
+            "in_data": { "type": 1 },
+            "api_seq": 0,
+            "run_job_id": "test2_runtime",
+            "final_result_callback": "http://172.16.1.110:19000/callback_post",
+            "service_dispatcher_uri": "http://172.16.1.110:18081",
+            "service_async_receiver_uri": "http://172.16.1.110:18082"
+        }
         '''
+        
+        # TODO: 위의 데이터로 1, 2, 3 등으로 나누어서 (post) service_dispatcher_uri로 호출하는 부분 만들기
+        
+        
+        
         
         print('[END] FlowInitiator')
         
