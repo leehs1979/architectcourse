4a3654d3c40b3d300679a4ee22ec5ef5f992a1eb
leehs1979 <leehs1979@naver.com>
2021-08-06 19:45:44 +0900

    Update using gunicorn

--- flowmanager/run.sh
+++ flowmanager/run.sh
@@ -1,4 +1,5 @@
 python manage.py makemigrations flowmanagerapi
 python manage.py migrate
 
-python manage.py runserver 0.0.0.0:28000
+#python manage.py runserver 0.0.0.0:28000
+gunicorn --bind 0.0.0.0:28000 --workers 4 --threads 8 --timeout 0 flowmanager 
