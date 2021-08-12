bdb779dcf832f0b2efa1888cdb7c1a162e1f3b78
leehs1979 <leehs1979@naver.com>
2021-08-13 08:05:25 +0900

    Update

--- flowmanager/run.sh
+++ flowmanager/run.sh
@@ -1,5 +1,5 @@
 python manage.py makemigrations flowmanagerapi
 python manage.py migrate
 
-#python manage.py runserver 0.0.0.0:28000
-gunicorn --bind 0.0.0.0:28000 --workers 4 --threads 8 --timeout 0 flowmanager.wsgi 
+python manage.py runserver 0.0.0.0:28000
+#gunicorn --bind 0.0.0.0:28000 --workers 4 --threads 8 --timeout 0 flowmanager.wsgi
